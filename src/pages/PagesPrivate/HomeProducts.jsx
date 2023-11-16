b9fc22f2132cff5e0e34d786facd550ea34c075b
yonatan78 <yonatansuarez78@gmail.com>
2023-11-15 20:19:15 -0500

    firist comnmit

---
+++ src/pages/PagesPrivate/HomeProducts.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavbarPrivate from '../../components/componentesPrivate/NavbarPrivate'
+
+function HomeProducts() {
+  return (
+    <>
+      <NavbarPrivate/>
+      Home Products
+    </>
+  )
+}
+
+export default HomeProducts
