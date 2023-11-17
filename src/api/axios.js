393d5854047b066f20e878360b2f388132314cfd
yonatan78 <yonatansuarez78@gmail.com>
2023-11-16 22:01:53 -0500

    solicitud api a vercel 1

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const instance =  axios.create({
-    baseURL: 'http://localhost:4000/api',
+    // baseURL: 'http://localhost:4000/api',
+    baseURL: 'https://online-store-backend-xi.vercel.app/api',
     withCredentials: true
 })
 
