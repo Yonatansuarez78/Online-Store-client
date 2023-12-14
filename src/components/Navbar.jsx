38d6e3c33a5ed87b6a5ffa61bbec0bcd68b33a0d
yonatan78 <yonatansuarez78@gmail.com>
2023-12-13 20:20:28 -0500

    agrego modificacion al footer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,9 +22,6 @@ function Navbar() {
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item ">
-                                <a className="nav-link active " aria-current="page" href="#">Blog</a>
-                            </li>
                             <li className="nav-item">
                                 <a className="nav-link active" aria-current="page" href="#">Servicios</a>
                             </li>
